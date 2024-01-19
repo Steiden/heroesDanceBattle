f6fb0455444ca4446942ad1bc21e9da15c1f5eb4
Steiden <baldindaniilza@mail.ru>
2024-01-19 17:32:01 +0500

    Remove js files from git

--- js/Enums.js
+++
@@ -1,6 +0,0 @@
-// Перечисления
-export var EnumHeroClass;
-(function (EnumHeroClass) {
-    EnumHeroClass["Knight"] = "\u0420\u044B\u0446\u0430\u0440\u044C";
-    EnumHeroClass["Mage"] = "\u041C\u0430\u0433";
-})(EnumHeroClass || (EnumHeroClass = {}));
